3284a175ddea8a8736742903ec8bfe0f46e802be
Sintayehu <sintemite@gmail.com>
2020-04-08 13:47:15 -0400

    blog update added

--- src/main/java/com/tts/TechTalentBlog/BlogPostController.java
+++ src/main/java/com/tts/TechTalentBlog/BlogPostController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -31,6 +32,11 @@ public class BlogPostController {
 		posts.add(savedPost);
 		model.addAttribute(savedPost);
 		return "blogpost/result.html";
+	}
+	@GetMapping("/posts/update/{id}")
+	public String updateForm(BlogPost blogPost, @PathVariable Long id, Model model) {
+		model.addAttribute("postIdToChange", id);
+		return "blogpost/update";
 	}
 	
 }
